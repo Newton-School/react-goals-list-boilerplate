0028b9633f69fe6e72e510798efba6e0b3c4148b
RaghavSabby <ragsabharwal369@gmail.com>
2023-02-28 11:11:15 +0530

    components moved to different files

--- src/components/App.js
+++ src/components/App.js
@@ -1,46 +1,11 @@
 import React, { useState } from 'react'
 import '../styles/App.css';
-
-const GoalForm = () => {
-  const [formData, setFormData] = useState({
-    goal: "",
-    by: ""
-  });
-
-  return (
-    <>
-      <h1>My Goals</h1>
-      <form>
-        <input 
-          type="text" 
-          name='goal' 
-          placeholder='Goal...' 
-        />
-        <input 
-          type="text"
-          name="by"
-          placeholder='By...'
-        />
-        <button>Add</button>
-      </form>
-    </>
-  )
-}
-
-const ListOfGoals = () => {
-  return (
-    <ul>
-      <li>
-          <p>My goal is to goalName, by goalDeadline.</p>
-        </li>
-    </ul>
-  );
-}
+import GoalForm from './GoalForm';
+import ListOfGoals from './ListOfGoals';
 
 const App = () => {
 
   const [allGoals, setAllGoals] = useState([]);
-
 
   return (
     <div id="main">
@@ -48,6 +13,7 @@ const App = () => {
       <ListOfGoals />
     </div>
   )
+  
 }
 
 
